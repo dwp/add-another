5c25d152ed8631f1e10a804714ef404ef5dbad01
Phil Swan <mail@philswan.co.uk>
2019-09-02 14:08:43 +0100

    Add HMCTS and MOJ patterns

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply for New Style Employment and Support Allowance',
 
   // Default port that prototype runs on
   port: '3000',
