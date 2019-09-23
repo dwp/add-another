555bd16c6eb72dca5a74b9093842f568166389e7
Phil Swan <mail@philswan.co.uk>
2019-09-23 17:13:23 +0100

    More progres

--- app/routes/3.js
+++ app/routes/3.js
@@ -44,6 +44,39 @@ module.exports = function (app) {
 
   })
 
+  // DELETE CONDITION 1
+
+  app.get('/3/delete-condition-1', function (req, res) {
+
+    var count = parseInt(req.session.data['count'], 10)
+    var conditionID = parseInt(req.session.data['conditionID'], 10)
+
+    res.render('3/other-conditions', {'count': (count-1), 'conditionID': (conditionID-1), 'deleted': (1) })
+
+  })
+
+  // DELETE CONDITION 2
+
+  app.get('/3/delete-condition-2', function (req, res) {
+
+    var count = parseInt(req.session.data['count'], 10)
+    var conditionID = parseInt(req.session.data['conditionID'], 10)
+
+    res.render('3/other-conditions', {'count': (count-1), 'conditionID': (conditionID-1), 'deleted': (2) })
+
+  })
+
+  // DELETE CONDITION 3
+
+  app.get('/3/delete-condition-3', function (req, res) {
+
+    var count = parseInt(req.session.data['count'], 10)
+    var conditionID = parseInt(req.session.data['conditionID'], 10)
+
+    res.render('3/other-conditions', {'count': (count-1), 'conditionID': (conditionID-1), 'deleted': (3) })
+
+  })
+
 
 
   // END OF VERSION A ROUTES
